94f382a146309094b6b928dc572a8e3b9574ee62
greenauction <greenauction1996@gmail.com>
2018-03-17 11:41:03 -0800

    first commit

--- app/src/main/java/com/example/shravanram/greenauction/PortalCreation.java
+++ app/src/main/java/com/example/shravanram/greenauction/PortalCreation.java
@@ -9,17 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import static java.lang.Integer.parseInt;
 
@@ -33,10 +28,6 @@ public class PortalCreation extends AppCompatActivity {
     private EditText iniprice;
     private Button createbut;
     private DatabaseReference mRef;
-    String date;
-    String time1;
-    private DatabaseReference timeRef;
-    private TextView setTime;
 
     String c="";
     int count=0;
@@ -54,33 +45,12 @@ public class PortalCreation extends AppCompatActivity {
         dropDown.setAdapter(adapter);
         produce = (EditText) findViewById(R.id.produce);
         location = (EditText) findViewById(R.id.location);
-        setTime=(TextView) findViewById(R.id.timeID);
-      //  count1=(TextView) findViewById(R.id.count);
+        //  count1=(TextView) findViewById(R.id.count);
         qty = (EditText) findViewById(R.id.qty);
         time = (EditText) findViewById(R.id.timeSlot);
         iniprice = (EditText) findViewById(R.id.iniPrice);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-      mRef = FirebaseDatabase.getInstance().getReference().child("count");
-        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); // Make sure user insert date into edittext in this format.
-        Date dateObject;
-
-        try{
-            String dob_var=(time.getText().toString());
-
-            dateObject = formatter.parse(dob_var);
-
-            date = new SimpleDateFormat("dd/MM/yyyy").format(dateObject);
-            time1 = new SimpleDateFormat("h:mmaa").format(dateObject);
-        }
-
-        catch (java.text.ParseException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            Log.i("E11111111111", e.toString());
-        }
-
-        Toast.makeText(getBaseContext(), date + time, Toast.LENGTH_LONG).show();
+        mRef = FirebaseDatabase.getInstance().getReference().child("count");
 
 
         createbut = (Button) findViewById(R.id.create);
@@ -98,23 +68,21 @@ public class PortalCreation extends AppCompatActivity {
 
                 c = "" + count;
 
-               mRef.setValue(c);
+                mRef.setValue(c);
                 mDatabase.child("auction").child(c).child("prod").setValue(prod);
                 mDatabase.child("auction").child(c).child("loc").setValue(loc);
                 mDatabase.child("auction").child(c).child("qty").setValue(q);
                 mDatabase.child("auction").child(c).child("weight").setValue(kg);
-                mDatabase.child("auction").child(c).child("deadline").setValue(date);
-                mDatabase.child("auction").child(c).child("time").setValue(time1);
+                mDatabase.child("auction").child(c).child("deadline").setValue(ts);
                 mDatabase.child("auction").child(c).child("initialbid").setValue(init_pri);
-             //   mDatabase.setValue(c);
+                //   mDatabase.setValue(c);
 
 
             }
         });
-       mRef.addValueEventListener(new ValueEventListener() {
+        mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
                 c= dataSnapshot.getValue().toString();
 
                 count++;
@@ -127,21 +95,11 @@ public class PortalCreation extends AppCompatActivity {
 
             }
         });
-       timeRef.addValueEventListener(new ValueEventListener() {
-           @Override
-           public void onDataChange(DataSnapshot dataSnapshot) {
-               timeRef= FirebaseDatabase.getInstance().getReference().child("auction").child(c).child("time");
-               String t= dataSnapshot.getValue().toString();
-               setTime.setText(t);
-           }
-
-           @Override
-           public void onCancelled(DatabaseError databaseError) {
-
-           }
-       })
 
 
     }
 
 }
+
+
+
