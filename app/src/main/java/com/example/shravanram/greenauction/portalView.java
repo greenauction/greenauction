2bac42984400604aeb16d523ba2bb09c2f0640a9
greenauction <greenauction1996@gmail.com>
2018-03-18 03:54:19 -0800

    first commit

---
+++ app/src/main/java/com/example/shravanram/greenauction/portalView.java
@@ -0,0 +1,13 @@
+package com.example.shravanram.greenauction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class portalView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portal_view);
+    }
+}
