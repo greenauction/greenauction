7319f329481f79911ce0f4fac80c7f7533a1da23
greenauction <37416582+greenauction@users.noreply.github.com>
2018-04-10 23:59:34 +0530

    RECENT COMMIT

---
+++ app/src/main/java/com/example/shravanram/greenauction/SecondClass.java
@@ -0,0 +1,22 @@
+package com.example.shravanram.greenauction;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.shravanram.greenauction.R;
+
+/**
+ * Created by namra_000 on 10-04-2018.
+ */
+
+public class SecondClass extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+
+    }
+
+}
