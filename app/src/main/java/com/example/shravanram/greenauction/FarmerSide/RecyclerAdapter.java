bde90b6582a5b01766257a3669b3dc5419f79c90
greenauction <37416582+greenauction@users.noreply.github.com>
2018-05-13 01:37:24 +0530

    final

--- app/src/main/java/com/example/shravanram/greenauction/FarmerSide/RecyclerAdapter.java
+++ app/src/main/java/com/example/shravanram/greenauction/FarmerSide/RecyclerAdapter.java
@@ -1,25 +1,25 @@
 package com.example.shravanram.greenauction.FarmerSide;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.example.shravanram.greenauction.DummyActivity;
 import com.example.shravanram.greenauction.R;
 import com.example.shravanram.greenauction.firebase_models.FarmerInfo;
 
 import java.util.List;
 
-/**
- * Created by Shravan ram on 3/27/2018.
- */
-
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyHoder>{
     List<FarmerInfo>  list;
     Context context;
+    int arr;
     public RecyclerAdapter(){
 
     }
@@ -30,17 +30,21 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyHode
     }
     @Override
     public MyHoder onCreateViewHolder(ViewGroup parent,int viewType){
+
         View view = LayoutInflater.from(context).inflate(R.layout.blog_row_bid,parent,false);
         MyHoder myHoder=new MyHoder(view);
         return myHoder;
     }
     @Override
     public int getItemCount(){
-        int arr=0;
+        arr=0;
         try{
             if(list.size()==0)
             {
+
                 arr=0;
+
+
             }
             else
             {
@@ -59,17 +63,19 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyHode
     {
         FarmerInfo mylist=list.get(position);
         holder.name.setText(mylist.getFname());
-
-        holder.price.setText("Rating "+ mylist.getRating());
-        holder.rating.setText(mylist.getPrice());
+        float rating_given=Float.parseFloat(mylist.getRating());
+        holder.rating.setRating(rating_given);
+        holder.price.setText(mylist.getPrice());
     }
     class MyHoder extends RecyclerView.ViewHolder{
-        TextView name,price,rating;
+        TextView name,price;
+        RatingBar rating;
         public MyHoder(View itemView){
             super(itemView);
             name=(TextView)itemView.findViewById(R.id.fname);
             price=(TextView)itemView.findViewById(R.id.price);
-            rating=(TextView)itemView.findViewById(R.id.rating);
+            rating=(RatingBar)itemView.findViewById(R.id.rating);
+
         }
     }
 }
